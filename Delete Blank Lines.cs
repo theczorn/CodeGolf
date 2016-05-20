7182eaeb51caa97bd16678b65c41faf6c5d3bbf7
Curt Zorn <theczorn@gmail.com>
2016-05-20 15:13:49 -0500

    sp mistake

--- Delete Blank Lines.cs
+++ Delete Blank Lines.cs
@@ -6,6 +6,6 @@ SYNOPSIS: Given an unordered wall of text, strip out any blank lines
 using System;
 class p{
 	static void Main(){
-		for(var x="";x!=null;x=Console.ReadLine())Console.Write(X!=""?x+"\n":"");
+		for(var x="";x!=null;x=Console.ReadLine())Console.Write(x!=""?x+"\n":"");
 	}
 }
